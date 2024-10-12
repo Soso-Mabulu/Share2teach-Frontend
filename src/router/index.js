196add3bd855f2d79d34df782588f3c7452b3d47
Soso-Mabulu <112823408+Soso-Mabulu@users.noreply.github.com>
2024-10-13 00:47:19 +0200

    Refactor branch name in CI/CD workflow and fix typo in router

--- src/router/index.js
+++ src/router/index.js
@@ -184,7 +184,7 @@ const router = createRouter({
       meta: { requiresAuth: true },
     },
 
-    // Admin routes
+    // Admin routess
     {
       path: '/admin-dashboard',
       name: 'AdminDashboardView',
