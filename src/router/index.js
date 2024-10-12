298e83059562c0261f13909cd655643e723eecfa
mapulaMobs53 <masekwamengmapula5@gmail.com>
2024-10-12 20:59:20 +0200

    Advanced Reports

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ import SignupView from '@/views/SignupView.vue';
 import PublicUserView from '@/views/PublicUserView.vue';
 import ForgotPasswordView from '@/views/ForgotPasswordView.vue';
 import ResetPasswordView from '@/views/ResetPasswordView.vue';
-import DetailedView from '@/views/DetailedView.vue';
 import SearchResultsView from '@/views/SearchResultsView.vue';
 import AllDocuments from '@/views/AllDocuments.vue';
 import FAQView from '@/views/FAQView.vue';
@@ -42,6 +41,7 @@ import AdminSubjectView from '@/views/Admin-SubjectView.vue';
 import AdminSelfDirectedlearningView from '@/views/Admin-SelfDirectedlearningView.vue';
 import AdminPanelView from '@/views/AdminPanelView.vue';
 import AdminMaintainUsersView from '@/views/Admin-MaintainUsersView.vue';
+import AdminAdvancedReportsView from '@/views/Admin-AdvancedReportsView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -81,12 +81,6 @@ const router = createRouter({
       path: '/reset-password',
       name: 'ResetPasswordView',
       component: ResetPasswordView,
-    },
-    {
-      path: '/detailed-view',
-      name: 'DetailedView',
-      component: DetailedView,
-      props: route => ({ activeEndpoint: route.params.endpoint }),
     },
     {
       path: '/search-results',
@@ -226,6 +220,11 @@ const router = createRouter({
       path: '/admin-maintain-users',
       name: 'AdminMaintainUsersView',
       component: AdminMaintainUsersView,
+    },
+    {
+      path: 'admin-advanced-reports',
+      name: 'AdminAdvancedReportsView',
+      component: AdminAdvancedReportsView,
     },
 
     // Educator routes
