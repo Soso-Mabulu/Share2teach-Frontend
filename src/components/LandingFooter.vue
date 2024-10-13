b7baaffff60732f72b45d0a7137cffc691a26a2b
Soso-Mabulu <112823408+Soso-Mabulu@users.noreply.github.com>
2024-10-14 01:31:48 +0200

    Add gsap dependency and enhance LandingNavbar UI

--- src/components/LandingFooter.vue
+++ src/components/LandingFooter.vue
@@ -1,119 +1,126 @@
 <template>
-  <footer class="bg-white">
-    <div class="container p-6 mx-auto">
-      <div class="lg:flex">
-        <div class="w-full -mx-6 lg:w-2/5">
-          <div class="px-6">
-            <router-link to="/">
-              <img class="w-auto h-7 rounded" src="@/assets/logo.jpg" alt="Share2Teach Logo" />
-              <span class="self-center text-1xl font-semibold whitespace-nowrap text-gray-800"
-                >SHARE2TEACH</span
-              >
-            </router-link>
+  <footer class="bg-zinc-50 text-surface/75 dark:bg-purple-900 dark:text-white/75 lg:text-left">
+    <!-- Top section with social icons -->
+    <div class="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
+      <div class="me-12 hidden lg:block">
+        <span>Get connected with us on social networks:</span>
+      </div>
+      <div class="flex justify-center">
+        <a href="#!" class="me-6 [&>svg]:h-4 [&>svg]:w-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 320 512">
+            <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/>
+          </svg>
+        </a>
+        <a href="#!" class="me-6 [&>svg]:h-4 [&>svg]:w-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">
+            <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/>
+          </svg>
+        </a>
+        <a href="#!" class="me-6 [&>svg]:h-4 [&>svg]:w-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512">
+            <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+          </svg>
+        </a>
+        <a href="#!" class="me-6 [&>svg]:h-4 [&>svg]:w-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512">
+            <path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/>
+          </svg>
+        </a>
+      </div>
+    </div>
 
-            <p class="max-w-sm mt-2 text-gray-600">
-              Join 31,000+ others and never miss out on new teaching tips, tutorials, and resources
-              to enhance your skills.
-            </p>
-
-            <div class="flex mt-6 -mx-2">
-              <a
-                href="#"
-                class="mx-2 text-gray-600 transition-colors duration-300 hover:text-blue-500"
-                aria-label="Reddit"
-              >
-                <!-- Reddit SVG -->
-                <svg
-                  class="w-5 h-5 fill-current"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C21.9939 17.5203 17.5203 21.9939 12 22ZM6.807 10.543C6.20862 10.5433 5.67102 10.9088 5.45054 11.465C5.23006 12.0213 5.37133 12.6558 5.807 13.066C5.92217 13.1751 6.05463 13.2643 6.199 13.33C6.18644 13.4761 6.18644 13.6229 6.199 13.769C6.199 16.009 8.814 17.831 12.028 17.831C15.242 17.831 17.858 16.009 17.858 13.769C17.8696 13.6229 17.8696 13.4761 17.858 13.33C18.4649 13.0351 18.786 12.3585 18.6305 11.7019C18.475 11.0453 17.8847 10.5844 17.21 10.593H17.157C16.7988 10.6062 16.458 10.7512 16.2 11C15.0625 10.2265 13.7252 9.79927 12.35 9.77L13 6.65L15.138 7.1C15.1931 7.60706 15.621 7.99141 16.131 7.992C16.1674 7.99196 16.2038 7.98995 16.24 7.986C16.7702 7.93278 17.1655 7.47314 17.1389 6.94094C17.1122 6.40873 16.6729 5.991 16.14 5.991C16.1022 5.99191 16.0645 5.99491 16.027 6C15.71 6.03367 15.4281 6.21641 15.268 6.492L12.82 6C12.7983 5.99535 12.7762 5.993 12.754 5.993C12.6094 5.99472 12.4851 6.09583 12.454 6.237L11.706 9.71C10.3138 9.7297 8.95795 10.157 7.806 10.939C7.53601 10.6839 7.17843 10.5422 6.807 10.543ZM12.18 16.524C12.124 16.524 12.067 16.524 12.011 16.524C11.955 16.524 11.898 16.524 11.842 16.524C11.0121 16.5208 10.2054 16.2497 9.542 15.751C9.49626 15.6958 9.47445 15.6246 9.4814 15.5533C9.48834 15.482 9.52348 15.4163 9.579 15.371C9.62737 15.3318 9.68771 15.3102 9.75 15.31C9.81233 15.31 9.87275 15.3315 9.921 15.371C10.4816 15.7818 11.159 16.0022 11.854 16C11.9027 16 11.9513 16 12 16C12.059 16 12.119 16 12.178 16C12.864 16.0011 13.5329 15.7863 14.09 15.386C14.1427 15.3322 14.2147 15.302 14.29 15.302C14.3653 15.302 14.4373 15.3322 14.49 15.386C14.5985 15.4981 14.5962 15.6767 14.485 15.786V15.746C13.8213 16.2481 13.0123 16.5208 12.18 16.523V16.524ZM14.307 14.08H14.291L14.299 14.041C13.8591 14.011 13.4994 13.6789 13.4343 13.2429C13.3691 12.8068 13.6162 12.3842 14.028 12.2269C14.4399 12.0697 14.9058 12.2202 15.1478 12.5887C15.3899 12.9572 15.3429 13.4445 15.035 13.76C14.856 13.9554 14.6059 14.0707 14.341 14.08H14.306H14.307ZM9.67 14C9.11772 14 8.67 13.5523 8.67 13C8.67 12.4477 9.11772 12 9.67 12C10.2223 12 10.67 12.4477 10.67 13C10.67 13.5523 10.2223 14 9.67 14Z"
-                  ></path>
-                </svg>
-              </a>
-
-              <a
-                href="#"
-                class="mx-2 text-gray-600 transition-colors duration-300 hover:text-blue-500"
-                aria-label="Facebook"
-              >
-                <!-- Facebook SVG -->
-                <svg
-                  class="w-5 h-5 fill-current"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M2.00195 12.002C2.00312 16.9214 5.58036 21.1101 10.439 21.881V14.892H7.90195V12.002H10.442V9.80204C10.3284 8.75958 10.6845 7.72064 11.4136 6.96698C12.1427 6.21332 13.1693 5.82306 14.215 5.90204C14.9655 5.91417 15.7141 5.98101 16.455 6.10205V8.56104H15.191C14.7558 8.50405 14.3183 8.64777 14.0017 8.95171C13.6851 9.25566 13.5237 9.68693 13.563 10.124V12.002H16.334L15.891 14.893H13.563V21.881C18.8174 21.0506 22.502 16.2518 21.9475 10.9611C21.3929 5.67041 16.7932 1.73997 11.502 1.55893C6.2108 1.3779 2.00195 5.39294 2.00195 12.002Z"
-                  ></path>
-                </svg>
-              </a>
-            </div>
-          </div>
+    <!-- Main content -->
+    <div class="mx-6 py-10 text-center md:text-left">
+      <div class="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+        <!-- Logo and description -->
+        <div class="">
+          <h6 class="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
+            <span class="me-3 [&>svg]:h-4 [&>svg]:w-4">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M12.378 1.602a.75.75 0 00-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03zM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 00.372-.648V7.93zM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 00.372.648l8.628 5.033z" />
+              </svg>
+            </span>
+            SHARE2TEACH
+          </h6>
+          <p class="mb-4">
+            Join 31,000+ educators and never miss out on new teaching tips, tutorials, and resources to enhance your skills.
+          </p>
+        </div>
+        
+        <!-- Quick Links -->
+        <div>
+          <h6 class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+            Quick Links
+          </h6>
+          <p class="mb-4">
+            <a href="#" class="text-surface/75 dark:text-white/75">About Us</a>
+          </p>
+          <p class="mb-4">
+            <a href="#" class="text-surface/75 dark:text-white/75">Courses</a>
+          </p>
+          <p class="mb-4">
+            <a href="#" class="text-surface/75 dark:text-white/75">Blog</a>
+          </p>
+          <p>
+            <a href="#" class="text-surface/75 dark:text-white/75">Contact</a>
+          </p>
         </div>
 
-        <div class="w-full mt-8 lg:mt-0 lg:w-3/5">
-          <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            <div>
-              <h4 class="font-bold text-gray-800">Resources</h4>
-              <ul class="mt-4 space-y-2">
-                <li>
-                  <router-link
-                    class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
-                    to="/about"
-                  >
-                    About Us
-                  </router-link>
-                </li>
-                <li>
-                  <router-link
-                    class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
-                    to="/contact"
-                  >
-                    Contact
-                  </router-link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h4 class="font-bold text-gray-800">Company</h4>
-              <ul class="mt-4 space-y-2">
-                <li>
-                  <router-link
-                    class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
-                    to="/terms"
-                  >
-                    Terms of Service
-                  </router-link>
-                </li>
-                <li>
-                  <router-link
-                    class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
-                    to="/privacy"
-                  >
-                    Privacy Policy
-                  </router-link>
-                </li>
-              </ul>
-            </div>
-            <div>
-            </div>
-          </div>
+        <!-- Resources -->
+        <div>
+          <h6 class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+            Resources
+          </h6>
+          <p class="mb-4">
+            <a href="#" class="text-surface/75 dark:text-white/75">Teaching Tips</a>
+          </p>
+          <p class="mb-4">
+            <a href="#" class="text-surface/75 dark:text-white/75">Lesson Plans</a>
+          </p>
+          <p class="mb-4">
+            <a href="#" class="text-surface/75 dark:text-white/75">Webinars</a>
+          </p>
+          <p>
+            <a href="#" class="text-surface/75 dark:text-white/75">Community Forum</a>
+          </p>
+        </div>
+
+        <!-- Newsletter -->
+        <div>
+          <h6 class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+            Newsletter
+          </h6>
+          <p class="mb-4">Stay updated with our latest news and offers.</p>
+          <form class="flex flex-col gap-4">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              class="rounded-md border border-neutral-300 bg-white px-4 py-2 text-surface/75 dark:border-neutral-600 dark:bg-neutral-700 dark:text-white/75"
+              required
+            />
+            <button
+              type="submit"
+              class="rounded-md bg-blue-600 px-4 py-2 text-white transition duration-300 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+              Subscribe
+            </button>
+          </form>
         </div>
       </div>
     </div>
-    <div class="py-4 text-center border-t border-gray-300">
-      <p class="text-sm text-gray-600">© 2024 Share2Teach. All Rights Reserved.</p>
+
+    <!-- Copyright section -->
+    <div class="bg-black/5 p-6 text-center dark:bg-white/5">
+      <span>© 2024 Copyright:</span>
+      <a class="font-semibold" href="#">Share2Teach</a>
     </div>
   </footer>
 </template>
 
-<script setup></script>
+<script setup>
+// No specific script required for now
+</script>
 
 <style scoped>
-/* Custom styles if necessary */
+/* Any additional styles can be added here if needed */
 </style>
