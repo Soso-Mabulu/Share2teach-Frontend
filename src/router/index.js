003f7780b5aa19573d6052203c117b3dd27b75a2
Soso-Mabulu <112823408+Soso-Mabulu@users.noreply.github.com>
2024-10-14 00:18:27 +0200

    Add @fortawesome/fontawesome-free dependency

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import TutoringServices from '@/views/TutoringServices.vue';
 import OnlineCourses from '@/views/OnlineCourses.vue';
 import StudentCounseling from '@/views/StudentCounseling.vue';
 import SearchResultsView from '@/components/SearchResults.vue';
+import ViewAllDocuments from '@/components/ViewAllDocuments.vue';
 
 
 
@@ -18,7 +19,6 @@ import ModeratorViewReportedView from '@/views/Moderator-ViewReportedView.vue';
 import ModeratorContributorsView from '@/views/Moderator-ContributorsView.vue';
 import ModeratorSearchResultsView from '@/views/Moderator-SearchResultsView.vue';
 import ModeratorUploadDocuments from '@/views/Moderator-UploadDocumentsView.vue';
-import ModeratorViewAllDocumentsView from '@/views/Moderator-ViewAllDocumentsView.vue';
 import ModeratorModerateDocumentsView from '@/views/Moderator-ModerateDocumentsView.vue';
 import ModeratorSubjectView from '@/views/Moderator-SubjectView.vue';
 import ModeratorSelfDirectedlearningView from '@/views/Moderator-SelfDirectedlearningView.vue';
@@ -127,6 +127,30 @@ const router = createRouter({
       name: 'StudentCounseling',
       component: StudentCounseling,
     },
+    {
+      path: '/documents/approved',
+      name: 'ViewAllDocuments',
+      component: ViewAllDocuments,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/documents/pending',
+      name: 'PendingDocuments', // Unique name
+      component: ViewAllDocuments,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/documents/reported',
+      name: 'ReportedDocuments', // Unique name
+      component: ViewAllDocuments,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/documents/denied',
+      name: 'DeniedDocuments', // Unique name
+      component: ViewAllDocuments,
+      meta: { requiresAuth: true },
+    },
     
 
     // Moderator routes
@@ -141,18 +165,6 @@ const router = createRouter({
       path: '/approved-documents',
       name: 'AllApprovedDocumentsView',
       component: ModeratorAllApprovedDocumentsView,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/pending-documents',
-      name: 'PendingDocuments',
-      component: ModeratorViewAllDocumentsView,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/approved-documents',
-      name: 'ApprovedDocuments',
-      component: ModeratorViewAllDocumentsView,
       meta: { requiresAuth: true },
     },
     {
